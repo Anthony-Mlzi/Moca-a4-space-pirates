f8b7c00f6a2222f07404b3ee5075df195814e1e7
amelia-mcmahon67 <amelia.mcmahon@mohawkcollege.ca>
2025-11-18 18:57:44 -0500

    Add Basic Info

--- Moca-a4-space-pirates/Moca-a4-space-pirates/Game.cs
+++ Moca-a4-space-pirates/Moca-a4-space-pirates/Game.cs
@@ -18,7 +18,8 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(800, 600);
+            Window.SetTitle("SPACE PIRATES");
         }
 
         /// <summary>
