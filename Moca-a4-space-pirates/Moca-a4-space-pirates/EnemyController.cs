3f2789afe6a4fcd52365c1e0eb94a24513540d18
Anthony-Mlzi <000982050@mohawkcollege.ca>
2025-11-18 19:13:49 -0500

    Add code comments in enemy controller.

--- Moca-a4-space-pirates/Moca-a4-space-pirates/EnemyController.cs
+++ Moca-a4-space-pirates/Moca-a4-space-pirates/EnemyController.cs
@@ -8,13 +8,15 @@ namespace Moca_a4_space_pirates
 {
     internal class EnemyController
     {
+        //variables
+
         public void setup()
         {
-
+            //runs once
         }
         public void update()
         {
-
+            //runs every frame
         }
     }
 }
